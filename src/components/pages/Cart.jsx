bdac42879bb3aa94a804fe7cd202ee0c35a6a023
Julfikar-Haidar <julfikar6262@gmai.com>
2020-08-04 17:29:47 +0600

    loop local storage product and show cart component

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -14,8 +14,8 @@ class Cart extends Component {
     componentDidMount(){
         console.log('11 line print');
         const productCollect = JSON.parse(window.localStorage.getItem('myProduct'))
-        console.log('13', productCollect);
-        this.setState = ({
+        console.log('22', productCollect);
+        this.setState({
             products: productCollect
            
         })
@@ -23,8 +23,9 @@ class Cart extends Component {
     }
 
     render() {
-
-        console.log(this.state.products)
+        const { products} = this.state
+      
+        // console.log('product from render',this.state.products)
         return (
             <div>
                 <Breadcrumb pageName="Cart" />
@@ -37,7 +38,7 @@ class Cart extends Component {
                                         <div className="col-12">
                                             {/* <!-- Cart Area Start --> */}
 
-                                            {this.state.products.map((pro, index) => (
+                                          
                                             <form action="#" className="form form--cart">
                                                 <div className="cart-table table-content table-responsive">
                                                     <table className="table">
@@ -51,7 +52,10 @@ class Cart extends Component {
                                                                 <th>remove</th>
                                                             </tr>
                                                         </thead>
+                                                        
                                                         <tbody>
+                                                        {products.map((pro, index) => (
+                                                            
                                                             <tr>
                                                                 <td>
                                                                     <a href="single-product.html"><img src="assets/img/fashion/product/1.jpg" alt="product" /></a>
@@ -66,25 +70,14 @@ class Cart extends Component {
                                                                 <td><strong>$16.00</strong></td>
                                                                 <td><a href="#"><i className="fa fa-trash remove"></i></a></td>
                                                             </tr>
-                                                            <tr>
-                                                                <td>
-                                                                    <a href="single-product.html"><img src="assets/img/fashion/product/12.jpg" alt="product"/></a>
-                                                        </td>
-                                                                    <td className="cart-product-price"><h3><a href="#">Blue shoe</a></h3></td>
-                                                                    <td><strong>$16.00</strong></td>
-                                                                    <td>
-                                                                        <div className="quantity">
-                                                                            <input type="number" className="quantity-input" name="qty" id="qty2" value="1" />
-                                                                            <div className="dec qtybutton"><i className="fa fa-angle-down"></i></div><div className="inc qtybutton"><i className="fa fa-angle-up"></i></div></div>
-                                                                    </td>
-                                                                    <td><strong>$16.00</strong></td>
-                                                                    <td><a href="#"><i className="fa fa-trash remove"></i></a></td>
-                                                    </tr>
+                                                       
+                                                    ))}
                                                 </tbody>
+                                                  
                                             </table>
                                         </div>
                                     </form>
-                                                 ))}
+                                               
 
                                                  {/* <!-- Cart Area End -->   */}
                                 </div>
